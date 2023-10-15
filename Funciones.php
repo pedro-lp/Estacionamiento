c8d32a575dad60cefc9a9c123f9fbab40ed1beca
pedro-lp <piter_pacheco@hotmail.com>
2023-10-15 10:47:44 -0600

    se crea archivo de funciones

---
+++ Funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+function RutaServer(){
+	$RutaS="/Estacionamiento";
+	return $RutaS;
+}
+function Detener($valor){
+	print_r("<prev>$valor</prev>");
+	exit();
+}
+function Detener2($valor){
+	print_r("<prev>$valor</prev>");
+}
+?>
